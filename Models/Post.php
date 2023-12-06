4ede136fb159fdc738266d55f670ddfae6f7b195
marco76tv <marco76tv@users.noreply.github.com>
2023-12-06 21:55:31 +0000

    Check & fix styling

--- Models/Post.php
+++ Models/Post.php
@@ -52,6 +52,7 @@ use Spatie\Sluggable\SlugOptions;
  * @property string|null     $linkable_type
  * @property int|null        $views_count
  * @property Model|\Eloquent $linkable
+ *
  * @method static Builder|Post newModelQuery()
  * @method static Builder|Post newQuery()
  * @method static Builder|Post query()
@@ -86,6 +87,7 @@ use Spatie\Sluggable\SlugOptions;
  * @method static Builder|Post whereUrlLang($value)
  * @method static Builder|Post whereUserId($value)
  * @method static Builder|Post whereViewsCount($value)
+ *
  * @mixin \Eloquent
  */
 class Post extends Model
