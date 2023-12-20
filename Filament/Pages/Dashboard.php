d042404a0dd804d49f6c55513fb1b9c9bc90d715
marco76tv <marco.sottana@gmail.com>
2023-12-20 16:02:32 +0100

    first

---
+++ Filament/Pages/Dashboard.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Modules\Lang\Filament\Pages;
+
+use Filament\Pages\Page;
+
+class Dashboard extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'lang::filament.pages.dashboard';
+}
