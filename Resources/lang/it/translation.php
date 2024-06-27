07567ca7f05cbbf0ebabd2eb924de1d60bbdfe14
marco <marco.sottana@gmail.com>
2024-06-27 08:24:23 +0200

    up

--- Resources/lang/it/translation.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'act' => [
-        'publish_item_trans' => 'pubblica modifiche riga',
-    ],
-];
--- bashscripts/update.sh
+++
@@ -1,5 +0,0 @@
-git config core.fileMode false
-git pull --force --rebase 
-rm composer.lock || echo "composer.lock not exists" 
-composer update -W 
-composer analyse
