465105727ab5cc547977630ec07a4457f442ba7b
marco76tv <marco.sottana@gmail.com>
2024-09-05 23:01:00 +0200

    up

---
+++ Models/Contracts/HasTranslationsContract.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Lang\Models\Contracts;
+
+interface HasTranslationsContract
+{
+    public function getTranslation(string $key, string $locale, bool $useFallbackLocale = true): mixed;
+
+    /**
+     * @param int|array|string|null $value
+     */
+    public function setTranslation(string $key, string $locale, $value): self;
+}
