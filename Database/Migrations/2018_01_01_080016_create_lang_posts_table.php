b5355d08478c92e75c25dde3b38158a749070847
marco <marco.sottana@gmail.com>
2024-10-08 17:07:24 +0200

    up

--- Database/Migrations/2018_01_01_080016_create_lang_posts_table.php
+++ Database/Migrations/2018_01_01_080016_create_lang_posts_table.php
@@ -11,7 +11,8 @@ use Modules\Xot\Database\Migrations\XotBaseMigration;
 /*
  * Class CreateLangPostsTable.
  */
-return new class extends XotBaseMigration {
+return new class extends XotBaseMigration
+{
     protected ?string $model_class = Post::class;
 
     /**
