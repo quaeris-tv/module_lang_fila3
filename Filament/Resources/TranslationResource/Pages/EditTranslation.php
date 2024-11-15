da0282fd1213adee72afa5dec5c9744a0a3d440d
marco76tv <marco76tv@users.noreply.github.com>
2024-11-15 09:11:46 +0000

    Check & fix styling

--- Filament/Resources/TranslationResource/Pages/EditTranslation.php
+++ Filament/Resources/TranslationResource/Pages/EditTranslation.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\Lang\Filament\Resources\TranslationResource\Pages;
 
-use Modules\Lang\Filament\Resources\TranslationResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Modules\Lang\Filament\Resources\TranslationResource;
 
 class EditTranslation extends EditRecord
 {
