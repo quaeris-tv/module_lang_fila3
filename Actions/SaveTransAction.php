4d9b5039a1871e14b70dd3f9d6cee4cb8129748b
marco76tv <marco.sottana@gmail.com>
2024-11-27 17:55:29 +0100

    up

--- Actions/SaveTransAction.php
+++ Actions/SaveTransAction.php
@@ -19,6 +19,9 @@ class SaveTransAction
     public function execute(string $key, int|string|array|null $data): void
     {
         $filename = app(GetTransPathAction::class)->execute($key);
+        if(!File::exists($filename)){
+            app(SaveArrayAction::class)->execute(data: [], filename: $filename);
+        }
         try {
             $cont = File::getRequire($filename);
         } catch (\Exception $e) {
