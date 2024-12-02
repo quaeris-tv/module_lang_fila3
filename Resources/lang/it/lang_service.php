d237c483d7eb9b173c7dab66bfff794f6a45c38a
Davide Tricella <dadi.tricella@gmail.com>
2024-12-02 15:39:26 +0100

    .

--- Resources/lang/it/lang_service.php
+++ Resources/lang/it/lang_service.php
@@ -1,110 +1,118 @@
-<?php return array (
-  'fields' => 
-  array (
-    'created_at' => 
-    array (
-      'fields' => 'created_at',
-      'label' => 
-      array (
-        'fields' => 'created_at',
-      ),
+<?php
+return array(
+    'fields' =>
+    array(
+        'created_at' =>
+        array(
+            'fields' => 'created_at',
+            'label' =>
+            array(
+                'fields' => 'created_at',
+            ),
+        ),
+        'values' =>
+        array(
+            'fields' => 'values',
+            'label' =>
+            array(
+                'fields' => 'values',
+            ),
+        ),
+        'value' =>
+        array(
+            'label' => 'value',
+        ),
+        'email' =>
+        array(
+            'label' => 'email',
+        ),
+        'password' =>
+        array(
+            'label' => 'password',
+        ),
+        'remember' =>
+        array(
+            'label' => 'remember',
+        ),
+        'assetType' =>
+        array(
+            'name' =>
+            array(
+                'label' => 'assetType.name',
+            ),
+        ),
+        'description' =>
+        array(
+            'label' => 'description',
+        ),
+        'parent_id' =>
+        array(
+            'label' => 'parent_id',
+        ),
+        'technician_name' =>
+        array(
+            'label' => 'technician_name',
+        ),
+        'serial_number' =>
+        array(
+            'label' => 'serial_number',
+        ),
+        'brand' =>
+        array(
+            'label' => 'brand',
+        ),
+        'model' =>
+        array(
+            'label' => 'model',
+        ),
+        'area' =>
+        array(
+            'name' =>
+            array(
+                'label' => 'area.name',
+            ),
+        ),
+        'save' =>
+        array(
+            'label' => 'save',
+        ),
+        'cancel' =>
+        array(
+            'label' => 'cancel',
+        ),
+        'creator' =>
+        array(
+            'name' =>
+            array(
+                'label' => 'creator.name',
+            ),
+        ),
     ),
-    'values' => 
-    array (
-      'fields' => 'values',
-      'label' => 
-      array (
-        'fields' => 'values',
-      ),
+    'actions' =>
+    array(
+        'save' =>
+        array(
+            'label' => 'save',
+        ),
+        'cancel' =>
+        array(
+            'label' => 'cancel',
+        ),
+        'authenticate' =>
+        array(
+            'label' => 'authenticate',
+        ),
+        'downloadExample' =>
+        array(
+            'label' => 'downloadExample',
+        ),
+        'create' =>
+        array(
+            'label' => 'create',
+        ),
+        'createAnother' =>
+        array(
+            'label' => 'createAnother',
+        ),
     ),
-    'value' => 
-    array (
-      'label' => 'value',
-    ),
-    'email' => 
-    array (
-      'label' => 'email',
-    ),
-    'password' => 
-    array (
-      'label' => 'password',
-    ),
-    'remember' => 
-    array (
-      'label' => 'remember',
-    ),
-    'assetType' => 
-    array (
-      'name' => 
-      array (
-        'label' => 'assetType.name',
-      ),
-    ),
-    'description' => 
-    array (
-      'label' => 'description',
-    ),
-    'parent_id' => 
-    array (
-      'label' => 'parent_id',
-    ),
-    'technician_name' => 
-    array (
-      'label' => 'technician_name',
-    ),
-    'serial_number' => 
-    array (
-      'label' => 'serial_number',
-    ),
-    'brand' => 
-    array (
-      'label' => 'brand',
-    ),
-    'model' => 
-    array (
-      'label' => 'model',
-    ),
-    'area' => 
-    array (
-      'name' => 
-      array (
-        'label' => 'area.name',
-      ),
-    ),
-    'save' => 
-    array (
-      'label' => 'save',
-    ),
-    'cancel' => 
-    array (
-      'label' => 'cancel',
-    ),
-  ),
-  'actions' => 
-  array (
-    'save' => 
-    array (
-      'label' => 'save',
-    ),
-    'cancel' => 
-    array (
-      'label' => 'cancel',
-    ),
-    'authenticate' => 
-    array (
-      'label' => 'authenticate',
-    ),
-    'downloadExample' => 
-    array (
-      'label' => 'downloadExample',
-    ),
-    'create' => 
-    array (
-      'label' => 'create',
-    ),
-    'createAnother' => 
-    array (
-      'label' => 'createAnother',
-    ),
-  ),
-);
+);
