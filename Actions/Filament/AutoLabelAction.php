f260a157884d8de686da6f06b59c4bbc62113bb7
marco76tv <marco.sottana@gmail.com>
2024-12-05 21:55:18 +0100

    .

--- Actions/Filament/AutoLabelAction.php
+++ Actions/Filament/AutoLabelAction.php
@@ -60,9 +60,9 @@ class AutoLabelAction
                 app(SaveTransAction::class)->execute($label_key, $label_value);
             }
             $component->label($label);
+        } else {
+            $component->label('FIX:'.$label_key);
         }
-
-        $component->label('FIX:'.$label_key);
 
         return $component;
     }
