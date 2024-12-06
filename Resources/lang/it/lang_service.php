e9715c925a0a0c676584765907b1c491138c7f55
marco76tv <marco.sottana@gmail.com>
2024-12-06 12:31:40 +0100

    .

--- Resources/lang/it/lang_service.php
+++ Resources/lang/it/lang_service.php
@@ -1,257 +1,100 @@
-<<<<<<< HEAD
-<?php return array (
-  'fields' => 
-  array (
-    'created_at' => 
-    array (
-      'fields' => 'created_at',
-      'label' => 
-=======
 <?php
-return array (
-  'fields' =>
-  array (
-    'created_at' =>
-    array (
-      'fields' => 'created_at',
-      'label' =>
->>>>>>> origin/v0.2.10
-      array (
-        'fields' => 'created_at',
-      ),
-    ),
-<<<<<<< HEAD
-    'values' => 
-    array (
-      'fields' => 'values',
-      'label' => 
-=======
-    'values' =>
-    array (
-      'fields' => 'values',
-      'label' =>
->>>>>>> origin/v0.2.10
-      array (
-        'fields' => 'values',
-      ),
-    ),
-<<<<<<< HEAD
-    'value' => 
-    array (
-      'label' => 'value',
-    ),
-    'email' => 
-    array (
-      'label' => 'email',
-    ),
-    'password' => 
-    array (
-      'label' => 'password',
-    ),
-    'remember' => 
-    array (
-      'label' => 'remember',
-    ),
-    'assetType' => 
-    array (
-      'name' => 
-=======
-    'value' =>
-    array (
-      'label' => 'value',
-    ),
-    'email' =>
-    array (
-      'label' => 'email',
-    ),
-    'password' =>
-    array (
-      'label' => 'password',
-    ),
-    'remember' =>
-    array (
-      'label' => 'remember',
-    ),
-    'assetType' =>
-    array (
-      'name' =>
->>>>>>> origin/v0.2.10
-      array (
-        'label' => 'assetType.name',
-      ),
-    ),
-<<<<<<< HEAD
-    'description' => 
-    array (
-      'label' => 'Descrizione',
-    ),
-    'parent_id' => 
-    array (
-      'label' => 'parent_id',
-    ),
-    'technician_name' => 
-    array (
-      'label' => 'technician_name',
-    ),
-    'serial_number' => 
-    array (
-      'label' => 'serial_number',
-    ),
-    'brand' => 
-    array (
-      'label' => 'Marca',
-    ),
-    'model' => 
-    array (
-      'label' => 'Modello',
-    ),
-    'area' => 
-    array (
-      'name' => 
-=======
-    'description' =>
-    array (
-      'label' => 'description',
-    ),
-    'parent_id' =>
-    array (
-      'label' => 'parent_id',
-    ),
-    'technician_name' =>
-    array (
-      'label' => 'technician_name',
-    ),
-    'serial_number' =>
-    array (
-      'label' => 'serial_number',
-    ),
-    'brand' =>
-    array (
-      'label' => 'brand',
-    ),
-    'model' =>
-    array (
-      'label' => 'model',
-    ),
-    'area' =>
-    array (
-      'name' =>
->>>>>>> origin/v0.2.10
-      array (
-        'label' => 'area.name',
-      ),
-    ),
-<<<<<<< HEAD
-    'save' => 
-    array (
-      'label' => 'save',
-    ),
-    'cancel' => 
-    array (
-      'label' => 'cancel',
-    ),
-    'creator' => 
-    array (
-      'name' => 
-=======
-    'save' =>
-    array (
-      'label' => 'save',
-    ),
-    'cancel' =>
-    array (
-      'label' => 'cancel',
-    ),
-    'creator' =>
-    array (
-      'name' =>
->>>>>>> origin/v0.2.10
-      array (
-        'label' => 'creator.name',
-      ),
-    ),
-<<<<<<< HEAD
-    'file' => 
-    array (
-      'label' => 'file',
-    ),
-    'updated_at' => 
-    array (
-      'label' => 'updated_at',
-    ),
-  ),
-  'actions' => 
-  array (
-    'save' => 
-    array (
-      'label' => 'save',
-    ),
-    'cancel' => 
-    array (
-      'label' => 'cancel',
-    ),
-    'authenticate' => 
-    array (
-      'label' => 'authenticate',
-    ),
-    'downloadExample' => 
-    array (
-      'label' => 'downloadExample',
-    ),
-    'create' => 
-    array (
-      'label' => 'create',
-    ),
-    'createAnother' => 
-    array (
-      'label' => 'createAnother',
-    ),
-    'request' => 
-    array (
-      'label' => 'request',
-    ),
-    'login' => 
-    array (
-      'label' => 'login',
-    ),
-  ),
-);
-=======
-  ),
-  'actions' =>
-  array (
-    'save' =>
-    array (
-      'label' => 'save',
-    ),
-    'cancel' =>
-    array (
-      'label' => 'cancel',
-    ),
-    'authenticate' =>
-    array (
-      'label' => 'authenticate',
-    ),
-    'downloadExample' =>
-    array (
-      'label' => 'downloadExample',
-    ),
-    'create' =>
-    array (
-      'label' => 'create',
-    ),
-    'createAnother' =>
-    array (
-      'label' => 'createAnother',
-    ),
-    'login' =>
-    array (
-      'label' => 'login',
-    ),
-    'request' =>
-    array (
-      'label' => 'request',
-    ),
-  ),
-);
->>>>>>> origin/v0.2.10
+
+declare(strict_types=1);
+
+return [
+    'fields' => [
+        'created_at' => [
+            'fields' => 'created_at',
+            'label' => [
+                'fields' => 'created_at',
+            ],
+        ],
+        'values' => [
+            'fields' => 'values',
+            'label' => [
+                'fields' => 'values',
+            ],
+        ],
+        'value' => [
+            'label' => 'value',
+        ],
+        'email' => [
+            'label' => 'email',
+        ],
+        'password' => [
+            'label' => 'password',
+        ],
+        'remember' => [
+            'label' => 'remember',
+        ],
+        'assetType' => [
+            'name' => [
+                'label' => 'assetType.name',
+            ],
+        ],
+        'description' => [
+            'label' => 'Descrizione',
+        ],
+        'parent_id' => [
+            'label' => 'parent_id',
+        ],
+        'technician_name' => [
+            'label' => 'technician_name',
+        ],
+        'serial_number' => [
+            'label' => 'serial_number',
+        ],
+        'brand' => [
+            'label' => 'Marca',
+        ],
+        'model' => [
+            'label' => 'Modello',
+        ],
+        'area' => [
+            'name' => [
+                'label' => 'area.name',
+            ],
+        ],
+        'save' => [
+            'label' => 'save',
+        ],
+        'cancel' => [
+            'label' => 'cancel',
+        ],
+        'creator' => [
+            'name' => [
+                'label' => 'creator.name',
+            ],
+        ],
+        'file' => [
+            'label' => 'file',
+        ],
+    ],
+    'actions' => [
+        'save' => [
+            'label' => 'save',
+        ],
+        'cancel' => [
+            'label' => 'cancel',
+        ],
+        'authenticate' => [
+            'label' => 'authenticate',
+        ],
+        'downloadExample' => [
+            'label' => 'downloadExample',
+        ],
+        'create' => [
+            'label' => 'create',
+        ],
+        'createAnother' => [
+            'label' => 'createAnother',
+        ],
+        'request' => [
+            'label' => 'request',
+        ],
+        'login' => [
+            'label' => 'login',
+        ],
+    ],
+];
