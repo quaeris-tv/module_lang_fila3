a0746996dacbabf208dd1ee844319be239980d3d
marco76tv <marco.sottana@gmail.com>
2024-12-06 21:00:03 +0100

    .

--- Resources/lang/it/lang_service.php
+++ Resources/lang/it/lang_service.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
@@ -98,3 +99,126 @@ return [
         ],
     ],
 ];
+=======
+<?php return array (
+  'fields' => 
+  array (
+    'created_at' => 
+    array (
+      'fields' => 'created_at',
+      'label' => 
+      array (
+        'fields' => 'created_at',
+      ),
+    ),
+    'values' => 
+    array (
+      'fields' => 'values',
+      'label' => 
+      array (
+        'fields' => 'values',
+      ),
+    ),
+    'value' => 
+    array (
+      'label' => 'value',
+    ),
+    'email' => 
+    array (
+      'label' => 'email',
+    ),
+    'password' => 
+    array (
+      'label' => 'password',
+    ),
+    'remember' => 
+    array (
+      'label' => 'remember',
+    ),
+    'assetType' => 
+    array (
+      'name' => 
+      array (
+        'label' => 'assetType.name',
+      ),
+    ),
+    'description' => 
+    array (
+      'label' => 'description',
+    ),
+    'parent_id' => 
+    array (
+      'label' => 'parent_id',
+    ),
+    'technician_name' => 
+    array (
+      'label' => 'technician_name',
+    ),
+    'serial_number' => 
+    array (
+      'label' => 'serial_number',
+    ),
+    'brand' => 
+    array (
+      'label' => 'brand',
+    ),
+    'model' => 
+    array (
+      'label' => 'model',
+    ),
+    'area' => 
+    array (
+      'name' => 
+      array (
+        'label' => 'area.name',
+      ),
+    ),
+    'save' => 
+    array (
+      'label' => 'save',
+    ),
+    'cancel' => 
+    array (
+      'label' => 'cancel',
+    ),
+    'creator' => 
+    array (
+      'name' => 
+      array (
+        'label' => 'creator.name',
+      ),
+    ),
+    'isActive' => 
+    array (
+      'label' => 'isActive',
+    ),
+  ),
+  'actions' => 
+  array (
+    'save' => 
+    array (
+      'label' => 'save',
+    ),
+    'cancel' => 
+    array (
+      'label' => 'cancel',
+    ),
+    'authenticate' => 
+    array (
+      'label' => 'authenticate',
+    ),
+    'downloadExample' => 
+    array (
+      'label' => 'downloadExample',
+    ),
+    'create' => 
+    array (
+      'label' => 'create',
+    ),
+    'createAnother' => 
+    array (
+      'label' => 'createAnother',
+    ),
+  ),
+);
+>>>>>>> b9328780 (fix(lang_service.php): add isActive field and label to the existing fields and labels)
