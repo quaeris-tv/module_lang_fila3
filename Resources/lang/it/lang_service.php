adf4a4a7bea4faded1739c49f42e0c735f447521
Malebestia <vair81@gmail.com>
2024-12-09 18:15:16 +0100

    fix(lang_service.php): update code formatting to conform to PSR-12 standards

--- Resources/lang/it/lang_service.php
+++ Resources/lang/it/lang_service.php
@@ -1,90 +1,123 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'fields' => [
-        'created_at' => [
-            'fields' => 'created_at',
-            'label' => 'Data di creazione',
-        ],
-        'values' => [
-            'fields' => 'values',
-            'label' => 'Valori',
-        ],
-        'value' => [
-            'label' => 'Valore',
-        ],
-        'email' => [
-            'label' => 'Email',
-        ],
-        'password' => [
-            'label' => 'Password',
-        ],
-        'remember' => [
-            'label' => 'Ricordami',
-        ],
-        'assetType' => [
-            'name' => [
-                'label' => 'Tipo di risorsa',
-            ],
-        ],
-        'description' => [
-            'label' => 'Descrizione',
-        ],
-        'parent_id' => [
-            'label' => 'ID genitore',
-        ],
-        'technician_name' => [
-            'label' => 'Nome tecnico',
-        ],
-        'serial_number' => [
-            'label' => 'Numero di serie',
-        ],
-        'brand' => [
-            'label' => 'Marca',
-        ],
-        'model' => [
-            'label' => 'Modello',
-        ],
-        'area' => [
-            'name' => [
-                'label' => 'Area',
-            ],
-        ],
-        'save' => [
-            'label' => 'Salva',
-        ],
-        'cancel' => [
-            'label' => 'Annulla',
-        ],
-        'creator' => [
-            'name' => [
-                'label' => 'Creatore',
-            ],
-        ],
-        'isActive' => [
-            'label' => 'Attivo',
-        ],
-    ],
-    'actions' => [
-        'save' => [
-            'label' => 'Salva',
-        ],
-        'cancel' => [
-            'label' => 'Annulla',
-        ],
-        'authenticate' => [
-            'label' => 'Autentica',
-        ],
-        'downloadExample' => [
-            'label' => 'Scarica esempio',
-        ],
-        'create' => [
-            'label' => 'Crea',
-        ],
-        'createAnother' => [
-            'label' => 'Crea un altro',
-        ],
-    ],
-];
+<?php return array (
+  'fields' => 
+  array (
+    'created_at' => 
+    array (
+      'fields' => 'created_at',
+      'label' => 'Data di creazione',
+    ),
+    'values' => 
+    array (
+      'fields' => 'values',
+      'label' => 'Valori',
+    ),
+    'value' => 
+    array (
+      'label' => 'Valore',
+    ),
+    'email' => 
+    array (
+      'label' => 'Email',
+    ),
+    'password' => 
+    array (
+      'label' => 'Password',
+    ),
+    'remember' => 
+    array (
+      'label' => 'Ricordami',
+    ),
+    'assetType' => 
+    array (
+      'name' => 
+      array (
+        'label' => 'Tipo di risorsa',
+      ),
+    ),
+    'description' => 
+    array (
+      'label' => 'Descrizione',
+    ),
+    'parent_id' => 
+    array (
+      'label' => 'ID genitore',
+    ),
+    'technician_name' => 
+    array (
+      'label' => 'Nome tecnico',
+    ),
+    'serial_number' => 
+    array (
+      'label' => 'Numero di serie',
+    ),
+    'brand' => 
+    array (
+      'label' => 'Marca',
+    ),
+    'model' => 
+    array (
+      'label' => 'Modello',
+    ),
+    'area' => 
+    array (
+      'name' => 
+      array (
+        'label' => 'Area',
+      ),
+    ),
+    'save' => 
+    array (
+      'label' => 'Salva',
+    ),
+    'cancel' => 
+    array (
+      'label' => 'Annulla',
+    ),
+    'creator' => 
+    array (
+      'name' => 
+      array (
+        'label' => 'Creatore',
+      ),
+    ),
+    'isActive' => 
+    array (
+      'label' => 'Attivo',
+    ),
+    'longitude' => 
+    array (
+      'label' => 'longitude',
+    ),
+    'latitude' => 
+    array (
+      'label' => 'latitude',
+    ),
+  ),
+  'actions' => 
+  array (
+    'save' => 
+    array (
+      'label' => 'Salva',
+    ),
+    'cancel' => 
+    array (
+      'label' => 'Annulla',
+    ),
+    'authenticate' => 
+    array (
+      'label' => 'Autentica',
+    ),
+    'downloadExample' => 
+    array (
+      'label' => 'Scarica esempio',
+    ),
+    'create' => 
+    array (
+      'label' => 'Crea',
+    ),
+    'createAnother' => 
+    array (
+      'label' => 'Crea un altro',
+    ),
+  ),
+);
