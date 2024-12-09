948968e5a9cb2c4144cf4ef5ab8c5bd11c7521d6
Malebestia <vair81@gmail.com>
2024-12-09 18:18:10 +0100

    rebase 1/1

--- Resources/lang/it/lang_service.php
+++ Resources/lang/it/lang_service.php
@@ -1,123 +1,102 @@
-<?php return array (
-  'fields' => 
-  array (
-    'created_at' => 
-    array (
-      'fields' => 'created_at',
-      'label' => 'Data di creazione',
-    ),
-    'values' => 
-    array (
-      'fields' => 'values',
-      'label' => 'Valori',
-    ),
-    'value' => 
-    array (
-      'label' => 'Valore',
-    ),
-    'email' => 
-    array (
-      'label' => 'Email',
-    ),
-    'password' => 
-    array (
-      'label' => 'Password',
-    ),
-    'remember' => 
-    array (
-      'label' => 'Ricordami',
-    ),
-    'assetType' => 
-    array (
-      'name' => 
-      array (
-        'label' => 'Tipo di risorsa',
-      ),
-    ),
-    'description' => 
-    array (
-      'label' => 'Descrizione',
-    ),
-    'parent_id' => 
-    array (
-      'label' => 'ID genitore',
-    ),
-    'technician_name' => 
-    array (
-      'label' => 'Nome tecnico',
-    ),
-    'serial_number' => 
-    array (
-      'label' => 'Numero di serie',
-    ),
-    'brand' => 
-    array (
-      'label' => 'Marca',
-    ),
-    'model' => 
-    array (
-      'label' => 'Modello',
-    ),
-    'area' => 
-    array (
-      'name' => 
-      array (
-        'label' => 'Area',
-      ),
-    ),
-    'save' => 
-    array (
-      'label' => 'Salva',
-    ),
-    'cancel' => 
-    array (
-      'label' => 'Annulla',
-    ),
-    'creator' => 
-    array (
-      'name' => 
-      array (
-        'label' => 'Creatore',
-      ),
-    ),
-    'isActive' => 
-    array (
-      'label' => 'Attivo',
-    ),
-    'longitude' => 
-    array (
-      'label' => 'longitude',
-    ),
-    'latitude' => 
-    array (
-      'label' => 'latitude',
-    ),
-  ),
-  'actions' => 
-  array (
-    'save' => 
-    array (
-      'label' => 'Salva',
-    ),
-    'cancel' => 
-    array (
-      'label' => 'Annulla',
-    ),
-    'authenticate' => 
-    array (
-      'label' => 'Autentica',
-    ),
-    'downloadExample' => 
-    array (
-      'label' => 'Scarica esempio',
-    ),
-    'create' => 
-    array (
-      'label' => 'Crea',
-    ),
-    'createAnother' => 
-    array (
-      'label' => 'Crea un altro',
-    ),
-  ),
-);
+<?php
+
+declare(strict_types=1);
+
+return [
+    'fields' => [
+        'created_at' => [
+            'fields' => 'created_at',
+            'label' => [
+                'fields' => 'created_at',
+            ],
+        ],
+        'values' => [
+            'fields' => 'values',
+            'label' => [
+                'fields' => 'values',
+            ],
+        ],
+        'value' => [
+            'label' => 'value',
+        ],
+        'email' => [
+            'label' => 'email',
+        ],
+        'password' => [
+            'label' => 'password',
+        ],
+        'remember' => [
+            'label' => 'remember',
+        ],
+        'assetType' => [
+            'name' => [
+                'label' => 'assetType.name',
+            ],
+        ],
+        'description' => [
+            'label' => 'Descrizione',
+        ],
+        'parent_id' => [
+            'label' => 'parent_id',
+        ],
+        'technician_name' => [
+            'label' => 'technician_name',
+        ],
+        'serial_number' => [
+            'label' => 'serial_number',
+        ],
+        'brand' => [
+            'label' => 'Marca',
+        ],
+        'model' => [
+            'label' => 'Modello',
+        ],
+        'area' => [
+            'name' => [
+                'label' => 'area.name',
+            ],
+        ],
+        'save' => [
+            'label' => 'save',
+        ],
+        'cancel' => [
+            'label' => 'cancel',
+        ],
+        'creator' => [
+            'name' => [
+                'label' => 'creator.name',
+            ],
+        ],
+        'file' => [
+            'label' => 'file',
+        'isActive' => [
+            'label' => 'isActive',
+        ],
+    ],
+    'actions' => [
+        'save' => [
+            'label' => 'save',
+        ],
+        'cancel' => [
+            'label' => 'cancel',
+        ],
+        'authenticate' => [
+            'label' => 'authenticate',
+        ],
+        'downloadExample' => [
+            'label' => 'downloadExample',
+        ],
+        'create' => [
+            'label' => 'create',
+        ],
+        'createAnother' => [
+            'label' => 'createAnother',
+        ],
+        'request' => [
+            'label' => 'request',
+        ],
+        'login' => [
+            'label' => 'login',
+        ],
+    ],
+];
