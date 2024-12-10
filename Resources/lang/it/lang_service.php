a8e9dfc4f3225969c46ba5de2558826cd947fe6e
marco76tv <marco.sottana@gmail.com>
2024-12-10 11:12:53 +0100

    .

--- Resources/lang/it/lang_service.php
+++ Resources/lang/it/lang_service.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
@@ -103,3 +104,144 @@ return [
         ],
     ],
 ];
+=======
+<?php return array (
+  'fields' => 
+  array (
+    'created_at' => 
+    array (
+      'fields' => 'created_at',
+      'label' => 'Data di creazione',
+    ),
+    'values' => 
+    array (
+      'fields' => 'values',
+      'label' => 'Valori',
+    ),
+    'value' => 
+    array (
+      'label' => 'Valore',
+    ),
+    'email' => 
+    array (
+      'label' => 'Email',
+    ),
+    'password' => 
+    array (
+      'label' => 'Password',
+    ),
+    'remember' => 
+    array (
+      'label' => 'Ricordami',
+    ),
+    'assetType' => 
+    array (
+      'name' => 
+      array (
+        'label' => 'Tipo di risorsa',
+      ),
+    ),
+    'description' => 
+    array (
+      'label' => 'Descrizione',
+    ),
+    'parent_id' => 
+    array (
+      'label' => 'ID genitore',
+    ),
+    'technician_name' => 
+    array (
+      'label' => 'Nome tecnico',
+    ),
+    'serial_number' => 
+    array (
+      'label' => 'Numero di serie',
+    ),
+    'brand' => 
+    array (
+      'label' => 'Marca',
+    ),
+    'model' => 
+    array (
+      'label' => 'Modello',
+    ),
+    'area' => 
+    array (
+      'name' => 
+      array (
+        'label' => 'Area',
+      ),
+    ),
+    'save' => 
+    array (
+      'label' => 'Salva',
+    ),
+    'cancel' => 
+    array (
+      'label' => 'Annulla',
+    ),
+    'creator' => 
+    array (
+      'name' => 
+      array (
+        'label' => 'Creatore',
+      ),
+    ),
+    'isActive' => 
+    array (
+      'label' => 'Attivo',
+    ),
+    'password_expires_at' => 
+    array (
+      'label' => 'Scadenza password',
+    ),
+    'email_verified_at' => 
+    array (
+      'label' => 'Email verificata il',
+    ),
+    'file' => 
+    array (
+      'label' => 'File',
+    ),
+    'updated_at' => 
+    array (
+      'label' => 'updated_at',
+    ),
+    'name' => 
+    array (
+      'label' => 'name',
+    ),
+    'id' => 
+    array (
+      'label' => 'id',
+    ),
+  ),
+  'actions' => 
+  array (
+    'save' => 
+    array (
+      'label' => 'Salva',
+    ),
+    'cancel' => 
+    array (
+      'label' => 'Annulla',
+    ),
+    'authenticate' => 
+    array (
+      'label' => 'Autentica',
+    ),
+    'downloadExample' => 
+    array (
+      'label' => 'Scarica esempio',
+    ),
+    'create' => 
+    array (
+      'label' => 'Crea',
+    ),
+    'createAnother' => 
+    array (
+      'label' => 'Crea un altro',
+    ),
+  ),
+);
+>>>>>>> 90d43f2 (up)
