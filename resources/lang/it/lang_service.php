3dcc696f221e0cbc135e56289ee2b7dab20ca51b
marco76tv <marco.sottana@gmail.com>
2025-01-01 22:28:12 +0100

    up

--- resources/lang/it/lang_service.php
+++ resources/lang/it/lang_service.php
@@ -21,5 +21,21 @@
     array (
       'label' => 'notes',
     ),
+    'end_date' => 
+    array (
+      'label' => 'end_date',
+    ),
+    'failed_job_ids' => 
+    array (
+      'label' => 'failed_job_ids',
+    ),
+    'competent_health_unit' => 
+    array (
+      'label' => 'competent_health_unit',
+    ),
+    'vat_number' => 
+    array (
+      'label' => 'vat_number',
+    ),
   ),
 );
