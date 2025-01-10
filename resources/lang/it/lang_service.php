8ca66b6614bf5a97f35f343920f7a09576f2b74b
marco76tv <marco76tv@users.noreply.github.com>
2025-01-10 07:35:13 +0000

    Check & fix styling

--- resources/lang/it/lang_service.php
+++ resources/lang/it/lang_service.php
@@ -1,46 +1,38 @@
-<?php 
-return array (
-  'fields' => 
-  array (
-    'geocomplete' => 
-    array (
-      'label' => 'geocomplete',
-    ),
-    'radius' => 
-    array (
-      'label' => 'radius',
-    ),
-    'unit' => 
-    array (
-      'label' => 'unit',
-    ),
-    'activity' => 
-    array (
-      'label' => 'activity',
-    ),
-    'notes' => 
-    array (
-      'label' => 'notes',
-    ),
-    'end_date' => 
-    array (
-      'label' => 'end_date',
-    ),
-    'failed_job_ids' => 
-    array (
-      'label' => 'failed_job_ids',
-    ),
-    'competent_health_unit' => 
-    array (
-      'label' => 'competent_health_unit',
-    ),
-    'vat_number' => 
-    array (
-      'label' => 'vat_number',
-    ),
-    'tax_code' => 
-    array (
-      'label' => 'tax_code',
-    ),
-  ),
-);
+<?php
+
+declare(strict_types=1);
+
+return [
+    'fields' => [
+        'geocomplete' => [
+            'label' => 'geocomplete',
+        ],
+        'radius' => [
+            'label' => 'radius',
+        ],
+        'unit' => [
+            'label' => 'unit',
+        ],
+        'activity' => [
+            'label' => 'activity',
+        ],
+        'notes' => [
+            'label' => 'notes',
+        ],
+        'end_date' => [
+            'label' => 'end_date',
+        ],
+        'failed_job_ids' => [
+            'label' => 'failed_job_ids',
+        ],
+        'competent_health_unit' => [
+            'label' => 'competent_health_unit',
+        ],
+        'vat_number' => [
+            'label' => 'vat_number',
+        ],
+        'tax_code' => [
+            'label' => 'tax_code',
+        ],
+    ],
+];
