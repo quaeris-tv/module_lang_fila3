08bf6e716751f934ba12d8370fee847ab992e037
marco76tv <marco76tv@users.noreply.github.com>
2025-01-15 16:24:49 +0000

    Check & fix styling

--- lang/it/txt.php
+++ lang/it/txt.php
@@ -1,24 +1,22 @@
-<?php return array (
-  'fields' => 
-  array (
-    'email' => 
-    array (
-      'label' => 'email',
-    ),
-    'password' => 
-    array (
-      'label' => 'password',
-    ),
-    'remember' => 
-    array (
-      'label' => 'remember',
-    ),
-  ),
-  'actions' => 
-  array (
-    'authenticate' => 
-    array (
-      'label' => 'authenticate',
-    ),
-  ),
-);
+<?php
+
+declare(strict_types=1);
+
+return [
+    'fields' => [
+        'email' => [
+            'label' => 'email',
+        ],
+        'password' => [
+            'label' => 'password',
+        ],
+        'remember' => [
+            'label' => 'remember',
+        ],
+    ],
+    'actions' => [
+        'authenticate' => [
+            'label' => 'authenticate',
+        ],
+    ],
+];
