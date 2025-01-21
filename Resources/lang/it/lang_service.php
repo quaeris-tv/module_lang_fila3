f3f8793286ba8759251c272a59c417caee7909f4
marco76tv <marco.sottana@gmail.com>
2025-01-21 08:28:16 +0100

    up

--- Resources/lang/it/lang_service.php
+++ Resources/lang/it/lang_service.php
@@ -1,46 +1,138 @@
-<?php 
-return array (
-  'fields' => 
-  array (
-    'geocomplete' => 
-    array (
-      'label' => 'geocomplete',
-    ),
-    'radius' => 
-    array (
-      'label' => 'radius',
-    ),
-    'unit' => 
-    array (
-      'label' => 'unit',
-    ),
-    'activity' => 
-    array (
-      'label' => 'activity',
-    ),
-    'notes' => 
-    array (
-      'label' => 'notes',
-    ),
-    'end_date' => 
-    array (
-      'label' => 'end_date',
-    ),
-    'failed_job_ids' => 
-    array (
-      'label' => 'failed_job_ids',
-    ),
-    'competent_health_unit' => 
-    array (
-      'label' => 'competent_health_unit',
-    ),
-    'vat_number' => 
-    array (
-      'label' => 'vat_number',
-    ),
-    'tax_code' => 
-    array (
-      'label' => 'tax_code',
-    ),
-  ),
-);
+<?php
+
+declare(strict_types=1);
+
+return [
+    'fields' => [
+        'created_at' => [
+            'fields' => 'created_at',
+            'label' => 'Data di creazione',
+        ],
+        'values' => [
+            'fields' => 'values',
+            'label' => 'Valori',
+        ],
+        'value' => [
+            'label' => 'Valore',
+        ],
+        'email' => [
+            'label' => 'Email',
+        ],
+        'password' => [
+            'label' => 'Password',
+        ],
+        'remember' => [
+            'label' => 'Ricordami',
+        ],
+        'assetType' => [
+            'name' => [
+                'label' => 'Tipo di risorsa',
+            ],
+        ],
+        'description' => [
+            'label' => 'Descrizione',
+        ],
+        'parent_id' => [
+            'label' => 'ID genitore',
+        ],
+        'technician_name' => [
+            'label' => 'Nome tecnico',
+        ],
+        'serial_number' => [
+            'label' => 'Numero di serie',
+        ],
+        'brand' => [
+            'label' => 'Marca',
+        ],
+        'model' => [
+            'label' => 'Modello',
+        ],
+        'area' => [
+            'name' => [
+                'label' => 'Area',
+            ],
+        ],
+        'save' => [
+            'label' => 'Salva',
+        ],
+        'cancel' => [
+            'label' => 'Annulla',
+        ],
+        'creator' => [
+            'name' => [
+                'label' => 'Creatore',
+            ],
+        ],
+        'isActive' => [
+            'label' => 'Attivo',
+        ],
+        'password_expires_at' => [
+            'label' => 'Scadenza password',
+        ],
+        'email_verified_at' => [
+            'label' => 'Email verificata il',
+        ],
+        'file' => [
+            'label' => 'File',
+        ],
+        'updated_at' => [
+            'label' => 'Ultima modifica',
+        ],
+        'name' => [
+            'label' => 'Nome',
+        ],
+        'id' => [
+            'label' => 'ID',
+        ],
+        'roles' => [
+            'name' => [
+                'label' => 'roles.name',
+            ],
+        ],
+        'role' => [
+            'name' => [
+                'label' => 'role.name',
+            ],
+        ],
+        'teams' => [
+            'name' => [
+                'label' => 'teams.name',
+            ],
+        ],
+        'applyFilters' => [
+            'label' => 'applyFilters',
+        ],
+        'toggleColumns' => [
+            'label' => 'toggleColumns',
+        ],
+        'reorderRecords' => [
+            'label' => 'reorderRecords',
+        ],
+        'resetFilters' => [
+            'label' => 'resetFilters',
+        ],
+        'openFilters' => [
+            'label' => 'openFilters',
+        ],
+    ],
+    'actions' => [
+        'save' => [
+            'label' => 'Salva',
+        ],
+        'cancel' => [
+            'label' => 'Annulla',
+        ],
+        'authenticate' => [
+            'label' => 'Autentica',
+        ],
+        'downloadExample' => [
+            'label' => 'Scarica esempio',
+        ],
+        'create' => [
+            'label' => 'Crea',
+        ],
+        'createAnother' => [
+            'label' => 'Crea un altro',
+        ],
+    ],
+];
