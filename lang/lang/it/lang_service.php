b8e5006172d7054903c6968a80f316c5e55166d4
marco76tv <marco.sottana@gmail.com>
2025-02-04 13:43:47 +0100

    up

--- lang/lang/it/lang_service.php
+++ lang/lang/it/lang_service.php
@@ -1,181 +1,136 @@
 <?php
-return array (
-  'fields' =>
-  array (
-    'created_at' =>
-    array (
-      'fields' => 'created_at',
-      'label' => 'Data di creazione',
-    ),
-    'values' =>
-    array (
-      'fields' => 'values',
-      'label' => 'Valori',
-    ),
-    'value' =>
-    array (
-      'label' => 'Valore',
-    ),
-    'email' =>
-    array (
-      'label' => 'Email',
-    ),
-    'password' =>
-    array (
-      'label' => 'Password',
-    ),
-    'remember' =>
-    array (
-      'label' => 'Ricordami',
-    ),
-    'assetType' =>
-    array (
-      'name' =>
-      array (
-        'label' => 'Tipo di risorsa',
-      ),
-    ),
-    'description' =>
-    array (
-      'label' => 'Descrizione',
-    ),
-    'parent_id' =>
-    array (
-      'label' => 'ID genitore',
-    ),
-    'technician_name' =>
-    array (
-      'label' => 'Nome tecnico',
-    ),
-    'serial_number' =>
-    array (
-      'label' => 'Numero di serie',
-    ),
-    'brand' =>
-    array (
-      'label' => 'Marca',
-    ),
-    'model' =>
-    array (
-      'label' => 'Modello',
-    ),
-    'area' =>
-    array (
-      'name' =>
-      array (
-        'label' => 'Area',
-      ),
-    ),
-    'save' =>
-    array (
-      'label' => 'Salva',
-    ),
-    'cancel' =>
-    array (
-      'label' => 'Annulla',
-    ),
-    'creator' =>
-    array (
-      'name' =>
-      array (
-        'label' => 'Creatore',
-      ),
-    ),
-    'isActive' =>
-    array (
-      'label' => 'Attivo',
-    ),
-    'password_expires_at' =>
-    array (
-      'label' => 'Scadenza password',
-    ),
-    'email_verified_at' =>
-    array (
-      'label' => 'Email verificata il',
-    ),
-    'file' =>
-    array (
-      'label' => 'File',
-    ),
-    'updated_at' =>
-    array (
-      'label' => 'Ultima modifica',
-    ),
-    'name' =>
-    array (
-      'label' => 'Nome',
-    ),
-    'id' =>
-    array (
-      'label' => 'ID',
-    ),
-    'roles' =>
-    array (
-      'name' =>
-      array (
-        'label' => 'roles.name',
-      ),
-    ),
-    'role' =>
-    array (
-      'name' =>
-      array (
-        'label' => 'role.name',
-      ),
-    ),
-    'teams' =>
-    array (
-      'name' =>
-      array (
-        'label' => 'teams.name',
-      ),
-    ),
-    'applyFilters' =>
-    array (
-      'label' => 'applyFilters',
-    ),
-    'toggleColumns' =>
-    array (
-      'label' => 'toggleColumns',
-    ),
-    'reorderRecords' =>
-    array (
-      'label' => 'reorderRecords',
-    ),
-    'resetFilters' =>
-    array (
-      'label' => 'resetFilters',
-    ),
-    'openFilters' =>
-    array (
-      'label' => 'openFilters',
-    ),
-  ),
-  'actions' =>
-  array (
-    'save' =>
-    array (
-      'label' => 'Salva',
-    ),
-    'cancel' =>
-    array (
-      'label' => 'Annulla',
-    ),
-    'authenticate' =>
-    array (
-      'label' => 'Autentica',
-    ),
-    'downloadExample' =>
-    array (
-      'label' => 'Scarica esempio',
-    ),
-    'create' =>
-    array (
-      'label' => 'Crea',
-    ),
-    'createAnother' =>
-    array (
-      'label' => 'Crea un altro',
-    ),
-  ),
-);
+
+return [
+    'fields' => [
+        'created_at' => [
+            'fields' => 'created_at',
+            'label' => 'Data di creazione',
+        ],
+        'values' => [
+            'fields' => 'values',
+            'label' => 'Valori',
+        ],
+        'value' => [
+            'label' => 'Valore',
+        ],
+        'email' => [
+            'label' => 'Email',
+        ],
+        'password' => [
+            'label' => 'Password',
+        ],
+        'remember' => [
+            'label' => 'Ricordami',
+        ],
+        'assetType' => [
+            'name' => [
+                'label' => 'Tipo di risorsa',
+            ],
+        ],
+        'description' => [
+            'label' => 'Descrizione',
+        ],
+        'parent_id' => [
+            'label' => 'ID genitore',
+        ],
+        'technician_name' => [
+            'label' => 'Nome tecnico',
+        ],
+        'serial_number' => [
+            'label' => 'Numero di serie',
+        ],
+        'brand' => [
+            'label' => 'Marca',
+        ],
+        'model' => [
+            'label' => 'Modello',
+        ],
+        'area' => [
+            'name' => [
+                'label' => 'Area',
+            ],
+        ],
+        'save' => [
+            'label' => 'Salva',
+        ],
+        'cancel' => [
+            'label' => 'Annulla',
+        ],
+        'creator' => [
+            'name' => [
+                'label' => 'Creatore',
+            ],
+        ],
+        'isActive' => [
+            'label' => 'Attivo',
+        ],
+        'password_expires_at' => [
+            'label' => 'Scadenza password',
+        ],
+        'email_verified_at' => [
+            'label' => 'Email verificata il',
+        ],
+        'file' => [
+            'label' => 'File',
+        ],
+        'updated_at' => [
+            'label' => 'Ultima modifica',
+        ],
+        'name' => [
+            'label' => 'Nome',
+        ],
+        'id' => [
+            'label' => 'ID',
+        ],
+        'roles' => [
+            'name' => [
+                'label' => 'roles.name',
+            ],
+        ],
+        'role' => [
+            'name' => [
+                'label' => 'role.name',
+            ],
+        ],
+        'teams' => [
+            'name' => [
+                'label' => 'teams.name',
+            ],
+        ],
+        'applyFilters' => [
+            'label' => 'applyFilters',
+        ],
+        'toggleColumns' => [
+            'label' => 'toggleColumns',
+        ],
+        'reorderRecords' => [
+            'label' => 'reorderRecords',
+        ],
+        'resetFilters' => [
+            'label' => 'resetFilters',
+        ],
+        'openFilters' => [
+            'label' => 'openFilters',
+        ],
+    ],
+    'actions' => [
+        'save' => [
+            'label' => 'Salva',
+        ],
+        'cancel' => [
+            'label' => 'Annulla',
+        ],
+        'authenticate' => [
+            'label' => 'Autentica',
+        ],
+        'downloadExample' => [
+            'label' => 'Scarica esempio',
+        ],
+        'create' => [
+            'label' => 'Crea',
+        ],
+        'createAnother' => [
+            'label' => 'Crea un altro',
+        ],
+    ],
+];
