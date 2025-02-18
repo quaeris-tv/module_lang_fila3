e980b43de850049e4aeb26995ac832d5d13d4215
marco76tv <marco76tv@users.noreply.github.com>
2025-02-18 13:28:06 +0000

    Check & fix styling

--- app/Casts/LangField.php
+++ app/Casts/LangField.php
@@ -12,9 +12,9 @@ class LangField implements CastsAttributes
     /**
      * Cast the given value.
      *
-     * @param  BaseModelLang  $model
-     * @param  string  $key
-     * @param  array  $attributes
+     * @param BaseModelLang $model
+     * @param string        $key
+     * @param array         $attributes
      */
     public function get($model, $key, $value, $attributes)
     {
@@ -24,11 +24,11 @@ class LangField implements CastsAttributes
     /**
      * Prepare the given value for storage.
      *
-     * @param  BaseModelLang  $model
-     * @param  string  $key
-     * @param  array  $attributes
-     * @param  string  $key
-     * @param  string  $value
+     * @param BaseModelLang $model
+     * @param string        $key
+     * @param array         $attributes
+     * @param string        $key
+     * @param string        $value
      */
     public function set($model, $key, $value, $attributes): array
     {
