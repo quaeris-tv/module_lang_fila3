16ed2cdca30ae462371d992a8a8854a4fef66a9b
marco76tv <marco.sottana@gmail.com>
2025-02-18 14:28:29 +0100

    Auto update submodule

--- app/Providers/LangServiceProvider.php
+++ app/Providers/LangServiceProvider.php
@@ -24,14 +24,12 @@ use Modules\Xot\Services\BladeService;
 use Webmozart\Assert\Assert;
 
 /**
- * Undocumented class.
+ * ---.
  */
 class LangServiceProvider extends XotBaseServiceProvider
 {
     public string $name = 'Lang';
-
     protected string $module_dir = __DIR__;
-
     protected string $module_ns = __NAMESPACE__;
 
     public function boot(): void
