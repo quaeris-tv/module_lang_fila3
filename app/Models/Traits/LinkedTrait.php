7e5a5bd2036aa72e0e3746259251d5987415366c
marco76tv <marco.sottana@gmail.com>
2025-02-25 11:43:05 +0100

    up

---
+++ app/Models/Traits/LinkedTrait.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * ---.
+ */
+
+declare(strict_types=1);
+
+namespace Modules\Lang\Models\Traits;
+
+// use Illuminate\Support\Facades\URL;
+// use Laravel\Scout\Searchable;
+
+use Modules\Lang\Models\Post;
+
+// per dizionario morph
+// ------ traits ---
+/**
+ * Modules\Lang\Models\Traits\LinkedTrait.
+ *
+ * @property \Modules\Xot\Contracts\UserContract|null $user
+ * @property Post                                     $post
+ */
+trait LinkedTrait
+{
+}
